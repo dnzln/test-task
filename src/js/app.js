7f914baa941328ee518f89e02ec1d40ff98c50cc
Dnzln <qvanoman@gmail.com>
2019-08-29 14:23:14 +0300

    Done task

--- src/js/app.js
+++ src/js/app.js
@@ -1,89 +1,24 @@
-import './common.js';
-import $ from 'jquery';
+window.onload = function() {
+  const copter = document.querySelector('.copter-img'),
+    textBlock = document.querySelector('.intro-text-block'),
+    coptercontainer = document.querySelector('body'),
+    copterCenterX = copter.offsetLeft + (copter.offsetWidth / 2),
+    copterCenterY = copter.offsetTop + (copter.offsetHeight / 2);
 
-$(document).ready(() => {
+  function getMousePos(xRef, yRef) {
+    let panelRect = coptercontainer.getBoundingClientRect();
+    return {
+      x: Math.floor(xRef - panelRect.left) /(panelRect.right-panelRect.left)*coptercontainer.offsetWidth,
+      y: Math.floor(yRef - panelRect.top) / (panelRect.bottom -panelRect.top) * coptercontainer.offsetHeight
+    };
+  }
 
-  /**
-   * Next button click
-   */
-  $('.footer__next_button').click((e) => {
-    e.preventDefault();
-
-    let currentStep = $('.steps__single.active').index() + 1;
-
-    switch(currentStep) {
-
-      case 1:
-        if($('.content__single--first .content__radio input:checked').length === 0) {
-          alert('gh');
-        } else {
-          moveToStep(1);
-          $('.footer__bottom').show();
-          $('.footer__prev').show();
-        }
-        break;
-        
-      case 2:
-        if(validateEmail($('#email').val())) {
-          submitForm();
-        }
-        
-        break;
-
-    }
+  document.body.addEventListener('mousemove', function(e) {
+    let mousePos = getMousePos(e.clientX, e.clientY),
+      distX = mousePos.x - copterCenterX,
+      distY = mousePos.y - copterCenterY;
+    textBlock.style.transform = 'translate('+(-1*distX)/15+'px,'+(-1*distY)/15+'px)';
+    copter.style.transform = 'translate('+(-1*distX)/20+'px,'+(-1*distY)/20+'px)';
   });
 
-  $('.footer__prev_button').click(() => {
-    let currentStep = $('.steps__single.active').index();
-    moveToStep(currentStep + 1, 'prev');
-  });
-
-
-  function moveToStep(step = 0, direction = 'next') {
-    
-    if(direction === 'next') {
-      $('.steps__single').removeClass('active');
-      $('.steps__single').eq(step).addClass('active');
-
-      $('.content__single').hide();
-      $('.content__single').eq(step).fadeIn();
-    } else {
-      $('.steps__single').removeClass('active');
-      $('.steps__single').eq(step - 2).addClass('active');
-
-      $('.content__single').hide();
-      $('.content__single').eq(step - 2).fadeIn();
-    }
-  }
-
-
-  function submitForm() {
-
-    let $form = $(this);
-
-    $.ajax({
-      type: $form.attr('method'),
-      url: $form.attr('action'),
-      data: $form.serialize()
-    }).done(function() {
-
-      moveToStep(3);
-
-    }).fail(function() {
-
-      console.log('fail');
-
-    });
-  }
-
-  function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-  }
-    
-
-  
-}); 
-
-
-
+};
